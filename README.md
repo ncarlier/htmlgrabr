1863fdf6cbdb0fd0c2f7811df7626e9bd05b6771
Nicolas Carlier <n.carlier@nunux.org>
2018-11-04 17:44:51 +0000

    initial publication

---
+++ README.md
@@ -0,0 +1,42 @@
+# HTMLGrabr library
+
+[![Travis](https://img.shields.io/travis/ncarlier/htmlgrabr.svg)](https://travis-ci.org/ncarlier/htmlgrabr)
+[![Donate](https://img.shields.io/badge/donate-paypal-blue.svg)](https://paypal.me/nunux)
+
+A Node.js library to grab and clean HTML content.
+
+### Features
+
+- Extract page content from an URL (`HTMLGrabr.grabURL(url: URL)`)
+- Extract page content from a string (`HTMLGrabr.grab(s: string)`)
+- Clean the page content:
+  - Extract main content using `node-readability`
+  - Extract text content using `html2plaintext`
+  - Remove link or image references of blacklisted sites
+  - Remove pixel tracker
+  - Remove unwanted attributes (by default: `class` and `id`)
+- Extract Open Graph properties
+
+### Usage
+
+```bash
+npm install --save htmlgrabr
+```
+
+The in your code:
+
+```javascript
+import HTMLGrabr from 'htmlgrabr'
+
+const url = new URL('http://example.com/foo.html')
+const page = async HTMLGrabr.grab(url)
+
+console.log('Page title:', page.title)
+console.log('Page illustration:', page.image)
+console.log('Page URL:', page.url)
+console.log('Page HTML content:', page.html)
+console.log('Page text content:', page.text)
+console.log('Page images:', page.images)
+```
+
+---
---
+++ src/htmlgrabr.ts
@@ -0,0 +1,101 @@
+import fetch, { Headers, Request } from 'node-fetch'
+import { URL } from 'url'
+import { promisify } from 'util'
+import * as DOM from './dom_handler'
+import { clean } from './dom_cleaner'
+
+const pretty = require('pretty')
+const h2p = require('html2plaintext')
+
+const readability = promisify(require('node-readability'))
+
+interface GrabberConfig {
+  debug?: boolean
+  headers?: Headers
+}
+
+interface GrabbedPage {
+  title: string
+  url: string | null
+  image: string | null
+  html: string
+  text: string
+  images: DOM.ImageMeta[]
+}
+
+const DefaultConfig: GrabberConfig = {
+  debug: false,
+  headers: new Headers({
+    'User-Agent': 'Mozilla/5.0 (compatible; HTMLGrabr/1.0)',
+  })
+}
+
+export default class HTMLGrabr {
+
+  config: GrabberConfig
+
+  constructor(config: GrabberConfig = DefaultConfig) {
+    this.config = { ...DefaultConfig, ...config }
+  }
+
+  /**
+   * Grabs the content of a page from HTML text.
+   * @param html a string that contains HTML code
+   * @returns a page object
+   */
+  async grab(html: string): Promise<GrabbedPage> {
+    // Use Readability.js to extract HTML content
+    const article = await readability(html)
+    const doc = article.document
+
+    // Extract base URL
+    const baseURL = DOM.extractBaseUrl(doc) || undefined
+
+    // Extract Open Graph propreties
+    const ogProps = DOM.extractOpenGraphProps(doc)
+
+    // Clean the DOM
+    clean(doc, { baseURL, debug: this.config.debug })
+
+    // Extract images
+    const images = DOM.extractImages(doc, ogProps.image)
+
+    // Backup HTML content (if Readability fails)
+    let content = doc.body.innerHTML
+    if (article.content) {
+      // Extract HTML content
+      content = article.content
+    }
+
+    return {
+      title: ogProps.title || article.title,
+      url: ogProps.url || baseURL,
+      image: ogProps.image,
+      html: pretty(content, { ocd: true }),
+      text: h2p(content),
+      images
+    }
+  }
+
+  /**
+   * Grabs the content of a remote HTML page.
+   * @param url the URL to fetch and process
+   * @returns a page object
+   */
+  async grabUrl(url: URL) {
+    const req = new Request(url.toString(), {
+      headers: this.config.headers,
+    })
+
+    const res = await fetch(req)
+    if (!res.ok) {
+      throw new Error(`bad status response: ${res.statusText}`)
+    }
+    const body = await res.text()
+    const result = await this.grab(body)
+    if (!result.url) {
+      result.url = url.toString()
+    }
+    return result
+  }
+}
